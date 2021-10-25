9d4e63b2d16345d9f2de995a068c88a5558fd2b7
luchobifa <lucianobifa@gmail.com>
2021-10-25 13:35:04 -0300

    Node version update

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from "./store/index";
 import axios from "axios";
@@ -14,9 +14,9 @@ axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
   <Provider store = {store}> 
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
