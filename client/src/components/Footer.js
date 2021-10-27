c3ccf71bac41b18683fd37cedbafec4014c80fd8
luchobifa <lucianobifa@gmail.com>
2021-10-27 17:05:52 -0300

    Faltas de ortografia corregidas

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(){
         <footer className = {styles.conteiner}>
             <div className = {styles.divAbout}>
                 <h4 className = {styles.about} >ABOUT</h4>
-                <p className = {styles.parrafo}>Este es mi proyecto indivudual desarrollado en 3 semanas para el bootcamp SoyHenry el cual consiste en una aplicacion mediante la cual se puede crear razas de perro y buscarlas mediante una API(TheDogApi), filtrarlas, ordenarlas y acceder a sus respectivos detalles.</p>
+                <p className = {styles.parrafo}>Este es mi proyecto individual desarrollado en 3 semanas para el bootcamp SoyHenry el cual consiste en una aplicacion mediante la cual se pueden crear razas de perro y buscarlas mediante una API(TheDogApi), filtrarlas, ordenarlas y acceder a sus respectivos detalles.</p>
             </div>
             <div className = {styles.divLista}>
                 <h4 className = {styles.about} >FRONT-END</h4>
